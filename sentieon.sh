10378a14fe127617b018a01c93e3aa0dc1645705
Omar-Abd-Elwahab <83937475+Omar-Abd-Elwahab@users.noreply.github.com>
2022-11-30 16:33:00 -0500

    Update sentieon.sh

--- sentieon.sh
+++ sentieon.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+#HiFi data variant calling
 bash /Sentieon/DNAscopeHiFiBeta0.4.pipeline/dnascope_HiFi.sh \
-	-r ref.fa \
-	-i file.bam \
+	-r /path/to/reference/ref.fa \
+	-i /path/to/bam/file.bam \
 	-m /Sentieon/DNAscopeHiFiBeta0.4.pipeline/DNAscopeHiFiBeta0.4.model \
 	-t 16 \
-	out.vcf.gz
+	/path/to/output/out.vcf.gz
+
+
+#Illumina data varint calling
+$sentieon driver -i /path/to/bam/file.bam \
+    -r ref.fa \ \
+    -t 16 --algo DNAscope \
+    --model SentieonDNAscopeModel1.1.model \
+    /path/to/output/sample_tmp.vcf.gz
+
+$sentieon driver -t 16 \
+     -r /path/to/reference/ref.fa \
+     --algo DNAModelApply \
+     --model SentieonDNAscopeModel1.1.model \
+     -v /path/to/output/sample_tmp.vcf.gz \
+     /path/to/output/out.vcf.gz
 
