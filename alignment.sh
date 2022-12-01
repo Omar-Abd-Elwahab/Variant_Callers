0d7a7d3e064308a5a275e3951fa72f82b4658147
Omar-Abd-Elwahab <83937475+Omar-Abd-Elwahab@users.noreply.github.com>
2022-12-01 13:31:08 -0500

    Update alignment.sh

--- alignment.sh
+++ alignment.sh
@@ -4,14 +4,14 @@
 # Alignment
 sentieon=/path/to/sentieon-genomics-202112.04/bin/sentieon
 
-$sentieon bwa mem -t 16 -K 10000000 -R '@RG\tID:003\tSM:HG003\tPL:ILLUMINA' -Y /path/to/reference/ref.fna \
-    /path/to/fastq-illumina/MPHG003_S2_L002_R1_001.fastq.gz /path/to/fastq-illumina/MPHG003_S2_L002_R2_001.fastq.gz | \
+$sentieon bwa mem -t 16 -K 10000000 -R '@RG\tID:003\tSM:HG003\tPL:ILLUMINA' -Y /path/to/reference/ref.fa \
+    /path/to/fastq-illumina/fq1.fastq.gz /path/to/fastq-illumina/fq2.fastq.gz | \
     $sentieon util sort -t 16 --sam2bam -o /path/to/bam_illumina/file_sorted.bam -i -
 
 # Duplicate marking
-$sentieon driver -r /path/to/reference/ref.fna -t 16 -i /path/to/bam_illumina/HG003_sorted.bam --algo LocusCollector \
-    --fun score_info /path/to/bam_illumina/HG003_score.txt.gz
-$sentieon driver -r /path/to/reference/ref.fna -t 16 -i /path/to/bam_illumina/HG003_sorted.bam --algo Dedup \
-    --score_info /path/to/bam_illumina/HG003_score.txt.gz \
-    /path/to/bam_illumina/HG003_deduped.bam
+$sentieon driver -r /path/to/reference/ref.fa -t 16 -i /path/to/bam_illumina/file_sorted.bam --algo LocusCollector \
+    --fun score_info /path/to/bam_illumina/file_score.txt.gz
+$sentieon driver -r /path/to/reference/ref.fa -t 16 -i /path/to/bam_illumina/file_sorted.bam --algo Dedup \
+    --score_info /path/to/bam_illumina/file_score.txt.gz \
+    /path/to/bam_illumina/file_deduped.bam
 
